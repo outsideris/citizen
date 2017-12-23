952f6834686fdf5e22fc76eac41ef4d7ab875540
Outsider <outsideris@gmail.com>
2017-12-24 01:50:14 +0900

    refactor to make file list with .gitignore

--- test/lib/module.spec.js
+++ test/lib/module.spec.js
@@ -13,13 +13,13 @@ describe('module\'s', () => {
     it('should make file list', async () => {
       const target = path.join(__dirname, '../fixture/module1');
       const result = await makeFileList(target);
-      expect(result).to.have.members(['b.js', 'c', 'c/d.js', 'c/e.js', 'README.md', 'a.js']);
+      expect(result).to.have.members(['b.js', 'c/d.js', 'c/e.js', 'README.md', 'a.js']);
     });
 
     it('should ignore files depends on .gitignore', async () => {
       const target = path.join(__dirname, '../fixture/module2');
       const result = await makeFileList(target);
-      expect(result).to.have.members(['b.js', 'a.js', 'c']);
+      expect(result).to.have.members(['b.js', 'a.js']);
     });
   });
 
