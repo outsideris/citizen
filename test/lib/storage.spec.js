a4049be9e22474f6e9d80a89916d00eb453e40fd
Outsider <outsideris@gmail.com>
2018-01-03 22:58:09 +0900

    check whether the module is exist or not

--- test/lib/storage.spec.js
+++ test/lib/storage.spec.js
@@ -5,21 +5,22 @@ const { promisify } = require('util');
 const AWS = require('aws-sdk');
 
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
-const { saveModule } = require('../../lib/storage');
+const { saveModule, hasModule } = require('../../lib/storage');
 
 const readFile = promisify(fs.readFile);
+s3.save = promisify(s3.putObject);
 s3.delete = promisify(s3.deleteObject);
 
-describe('storage\'s', () => {
+describe('storage\'s', async () => {
+  const modulePath = `citizen/${(new Date()).getTime()}/test.tar.gz`;
+  const tarballPath = path.join(__dirname, '..', 'fixture/test.tar.gz');
+  let moduleBuf;
+
+  before(async () => {
+    moduleBuf = await readFile(tarballPath);
+  });
+
   describe('saveModule()', () => {
-    let moduleBuf;
-    const modulePath = `citizen/${(new Date()).getTime()}/test.tar.gz`;
-
-    before(async () => {
-      const tarballPath = path.join(__dirname, '..', 'fixture/test.tar.gz');
-      moduleBuf = await readFile(tarballPath);
-    });
-
     after(async () => {
       const params = {
         Bucket: process.env.AWS_S3_BUCKET,
@@ -31,6 +32,35 @@ describe('storage\'s', () => {
     it('should save the module onto S3', async () => {
       const result = await saveModule(modulePath, moduleBuf);
       expect(result).to.have.property('ETag');
+    });
+  });
+
+  describe('hasModule()', () => {
+    before(async () => {
+      const params = {
+        Bucket: process.env.AWS_S3_BUCKET,
+        Key: modulePath,
+        Body: moduleBuf,
+      };
+      await s3.save(params);
+    });
+
+    after(async () => {
+      const params = {
+        Bucket: process.env.AWS_S3_BUCKET,
+        Key: modulePath,
+      };
+      await s3.delete(params);
+    });
+
+    it('should return true if the module is already exist', async () => {
+      const exist = await hasModule(modulePath);
+      expect(exist).to.be.true;
+    });
+
+    it('should return false if the module is not already exist', async () => {
+      const exist = await hasModule(`${modulePath}/wrong`);
+      expect(exist).to.be.false;
     });
   });
 });
