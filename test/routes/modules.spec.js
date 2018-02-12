b120850c63f53c4d926f4d93647880158f1880b6
Outsider <outsideris@gmail.com>
2018-02-12 12:10:06 +0900

    remove a test which is not implemented

--- test/routes/modules.spec.js
+++ test/routes/modules.spec.js
@@ -63,14 +63,3 @@ describe('POST /v1/modules/:namespace/:name/:provider/:version', () => {
         expect(res.body.modules[0]).to.have.property('id').to.equal(modulePath);
       }));
 });
-
-describe('GET /v1/modules/:namespace/:name/:provider/versions', () => {
-  it('should return as application/json', () =>
-    request(app)
-      .get('/v1/modules/hashicorp/consul/aws/versions')
-      .expect('Content-Type', /application\/json/)
-      .expect(200)
-      .then((res) => {
-        expect(res.body).to.have.property('modules').to.be.an('array');
-      }));
-});
