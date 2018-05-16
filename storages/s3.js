f4170fc8b167707dcd7f152b05eba5f04d2d8dc3
Outsider <outsideris@gmail.com>
2018-05-16 23:57:41 +0900

    enhance s3 storage
    Signed-off-by: Outsider <outsideris@gmail.com>

--- storages/s3.js
+++ storages/s3.js
@@ -6,13 +6,14 @@ const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
 const S3_BUCKET = process.env.AWS_S3_BUCKET;
 if (!S3_BUCKET || !process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
-  throw new Error('AWS_S3_BUCKET, AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are secret are required.');
+  throw new Error('S3 storage require AWS_S3_BUCKET, AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY.');
 }
 
 s3.save = promisify(s3.putObject);
 s3.get = promisify(s3.getObject);
 
 module.exports = {
+  type: () => 's3',
   saveModule: async (path, tarball) => {
     debug(`save the module into ${path}.`);
 
