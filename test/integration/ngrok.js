f58749aaf36130c6b01db9a829d0478f113fbcdd
Outsider <outsideris@gmail.com>
2022-04-03 21:18:00 +0900

    test: add debug log for ci
    Signed-off-by: Outsider <outsideris@gmail.com>

--- test/integration/ngrok.js
+++ test/integration/ngrok.js
@@ -5,16 +5,17 @@ const debug = Debug('citizen:test:integration');
 
 const connect = async (port) => {
   debug('try to connect ngrok', port);
-  debug(process.env.NGROK_AUTHTOKEN.substr(0, 3));
 
   const url = await ngrok.connect({
     addr: port,
     authtoken: process.env.NGROK_AUTHTOKEN,
   });
+  debug('ngrok connected', url.host);
   return new URL(url);
 };
 
 const disconnect = async () => {
+  debug('ngrok disconnected');
   await ngrok.disconnect();
   await ngrok.kill();
 };
