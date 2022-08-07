0398750c77f915b641308bbd23659434b8b9e1d5
Outsider <outsideris@gmail.com>
2022-08-08 01:38:37 +0900

    store: set dedicate client for sqlite
    Signed-off-by: Outsider <outsideris@gmail.com>

--- stores/sqlite.js
+++ stores/sqlite.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client/sqlite');
 const debug = require('debug')('citizen:server:store:sqlite');
 
 const storeType = 'sqlite';
