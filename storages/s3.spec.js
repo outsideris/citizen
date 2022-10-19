aeda82923d0ffa1d1e1ae53aa9c2f5fab7d979db
Outsider <outsideris@gmail.com>
2022-10-20 02:29:48 +0900

    test: test s3 with mock library
    Signed-off-by: Outsider <outsideris@gmail.com>

--- storages/s3.spec.js
+++ storages/s3.spec.js
@@ -2,91 +2,57 @@ const path = require('node:path');
 const fs = require('node:fs');
 const { expect } = require('chai');
 const { promisify } = require('node:util');
-const { S3Client, PutObjectCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
+const { Readable } = require('node:stream');
+const { S3Client, PutObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3');
+const { mockClient } = require('aws-sdk-client-mock');
 
-const helper = require('../test/helper');
 const s3 = require('./s3');
 
-const s3client = new S3Client({});
+const s3Mock = mockClient(S3Client);
 const readFile = promisify(fs.readFile);
 
 describe("s3's", async () => {
   const modulePath = `citizen/${new Date().getTime()}/module.tar.gz`;
-  const tarballPath = path.join(__dirname, '../test', 'fixture/module.tar.gz');
-  let moduleBuf;
 
-  before(async () => {
-    helper.enableMock({ modulePath: `modules/${modulePath}` });
-    moduleBuf = await readFile(tarballPath);
-  });
-
-  after(() => {
-    helper.clearMock();
+  afterEach(() => {
+    s3Mock.reset();
   });
 
   describe('saveModule()', () => {
-    after(async () => {
-      const params = {
-        Bucket: process.env.CITIZEN_AWS_S3_BUCKET,
-        Key: `modules/${modulePath}`,
-      };
-      await s3client.send(new DeleteObjectCommand(params));
-    });
-
     it('should save the module onto S3', async () => {
+      s3Mock.on(PutObjectCommand).resolves({ ETag: '1234' });
+
+      const tarballPath = path.join(__dirname, '../test', 'fixture/module.tar.gz');
+      const moduleBuf = await readFile(tarballPath);
+
       const result = await s3.saveModule(modulePath, moduleBuf);
       expect(result).to.be.true;
     });
   });
 
   describe('hasModule()', () => {
-    before(async () => {
-      const params = {
-        Bucket: process.env.CITIZEN_AWS_S3_BUCKET,
-        Key: `modules/${modulePath}`,
-        Body: moduleBuf,
-      };
-      await s3client.send(new PutObjectCommand(params));
-    });
-
-    after(async () => {
-      const params = {
-        Bucket: process.env.CITIZEN_AWS_S3_BUCKET,
-        Key: `modules/${modulePath}`,
-      };
-      await s3client.send(new DeleteObjectCommand(params));
-    });
-
     it('should return true if the module is already exist', async () => {
+      s3Mock.on(GetObjectCommand).resolves({ Body: 'data' });
+
       const exist = await s3.hasModule(modulePath);
       expect(exist).to.be.true;
     });
 
     it('should return false if the module is not already exist', async () => {
+      s3Mock.on(GetObjectCommand).rejects({ name: 'NoSuchKey' });
+
       const exist = await s3.hasModule(`${modulePath}/wrong`);
       expect(exist).to.be.false;
     });
   });
 
   describe('getModule()', () => {
-    before(async () => {
-      const params = {
-        Bucket: process.env.CITIZEN_AWS_S3_BUCKET,
-        Key: `modules/${modulePath}`,
-        Body: moduleBuf,
-      };
-      await s3client.send(new PutObjectCommand(params));
-    });
-
-    after(async () => {
-      const params = {
-        Bucket: process.env.CITIZEN_AWS_S3_BUCKET,
-        Key: `modules/${modulePath}`,
-      };
-      await s3client.send(new DeleteObjectCommand(params));
-    });
-
     it('should get file buffer from S3', async () => {
+      const buf = Buffer.from('data');
+      s3Mock.on(GetObjectCommand).resolves({
+        Body: Readable.from(buf),
+      });
+
       const result = await s3.getModule(modulePath);
       expect(result).to.be.an.instanceof(Buffer);
     });
