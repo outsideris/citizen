c17859c754e12b3d579968327dacab5144d0bd0a
Outsider <outsideris@gmail.com>
2022-10-30 19:36:16 +0900

    feat(storage): unify gs storage APIs
    Signed-off-by: Outsider <outsideris@gmail.com>

--- storages/gs.js
+++ storages/gs.js
@@ -11,66 +11,59 @@ if (process.env.CITIZEN_STORAGE === 'gs' && !GS_BUCKET && !GS_KEYPATH) {
 }
 
 const gs = new Storage({
-  keyFile: 'key.json',
+  keyFile: GS_KEYPATH,
 });
 
-async function saveFile(type, path, tarball) {
-  if (!path) {
-    throw new Error('path is required.');
-  }
-  if (!tarball) {
-    throw new Error('tarball is required.');
-  }
-
-  const file = gs.bucket(GS_BUCKET).file(`${type}/${path}`);
-  const result = await file.save(tarball, (err) => {
-    if (!err) {
-      debug('saved');
-      return true;
-    }
-    return false;
-  });
-
-  return !!result;
-}
-
-async function hasFile(type, path) {
-  try {
-    const [resource] = await gs.bucket(GS_BUCKET).file(`${type}/${path}`).getMetadata();
-    if (resource.name) {
-      debug(`the ${type} already exist: ${path}.`);
-      return true;
-    }
-  } catch (err) {
-    // i think error name should be fixed
-    if (err.code === 404) {
-      debug(`the ${type} doesn't exist: ${path}.`);
-      return false;
-    }
-
-    throw err;
-  }
-
-  debug(`the ${type} doesn't exist: ${path}.`);
-  return false;
-}
-
-async function getFile(type, path) {
-  const file = await gs.bucket(GS_BUCKET).file(`${type}/${path}`);
-
-  return file
-    .download({
-      // don't set destination here
-    })
-    .then((data) => data[0]);
-}
-
-module.exports = {
+const gs = {
   type: () => 'gs',
-  saveModule: async (path, tarball) => saveFile('module', path, tarball),
-  saveProvider: async (path, tarball) => saveFile('provider', path, tarball),
-  hasModule: async (path) => hasFile('module', path),
-  getModule: async (path) => getFile('module', path),
-  hasProvider: async (path) => hasFile('provider', path),
-  getProvider: async (path) => getFile('provider', path),
-};
+  setItem: async (path, tarball) => {
+    if (!path) {
+      throw new Error('path is required.');
+    }
+    if (!tarball) {
+      throw new Error('tarball is required.');
+    }
+
+    const file = gs.bucket(GS_BUCKET).file(path);
+    const result = await file.save(tarball, (err) => {
+      if (!err) {
+        debug('saved');
+        return true;
+      }
+      return false;
+    });
+
+    return !!result;
+  },
+  hasItem: async (path) => {
+    try {
+      const [resource] = await gs.bucket(GS_BUCKET).file(path).getMetadata();
+      if (resource.name) {
+        debug(`the item already exist: ${path}.`);
+        return true;
+      }
+    } catch (err) {
+      // i think error name should be fixed
+      if (err.code === 404) {
+        debug(`the item doesn't exist: ${path}.`);
+        return false;
+      }
+
+      throw err;
+    }
+
+    debug(`the item doesn't exist: ${path}.`);
+    return false;
+  },
+  getItem: async (path) => {
+    const file = await gs.bucket(GS_BUCKET).file(path);
+
+    return file
+      .download({
+        // don't set destination here
+      })
+      .then((data) => data[0]);
+  },
+}
+
+module.exports = gs;
