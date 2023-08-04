9c712022c069be99a98e43cf929ec564d87f5a86
locke.song <locke.song@kakaobank.com>
2023-08-04 10:58:26 +0900

    Add minio support

--- storages/s3.js
+++ storages/s3.js
@@ -1,14 +1,31 @@
 const { S3Client, GetObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3');
 const debug = require('debug')('citizen:server');
 
-const s3client = new S3Client({});
+const MINIO_ENDPOINT = process.env.CITIZEN_STORAGE_ENDPOINT;
+if (process.env.CITIZEN_STORAGE === 'minio' && !MINIO_ENDPOINT) {
+  throw new Error('Minio storage requires CITIZEN_STORAGE_ENDPOINT.');
+}
 
 const S3_BUCKET = process.env.CITIZEN_STORAGE_BUCKET;
-if (process.env.CITIZEN_STORAGE === 's3' && !S3_BUCKET) {
+if ((process.env.CITIZEN_STORAGE === 's3' || process.env.CITIZEN_STORAGE === 'minio') && !S3_BUCKET) {
   throw new Error(
-    'S3 storage requires CITIZEN_STORAGE_BUCKET. Additionally, ensure that either AWS_PROFILE or AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are set. If running on AWS EC2 or ECS, IAM Roles may be used.',
+    'S3/Minio storage requires CITIZEN_STORAGE_BUCKET. Additionally, ensure that either AWS_PROFILE or AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are set. If running on AWS EC2 or ECS, IAM Roles may be used.',
   );
 }
+
+const s3client = (() => {
+  if (process.env.CITIZEN_STORAGE === 's3') {
+    return new S3Client({});
+  }
+  if (process.env.CITIZEN_STORAGE === 'minio') {
+    return new S3Client({
+      endpoint: MINIO_ENDPOINT,
+      forcePathStyle: true,
+      region: 'no-region',
+    });
+  }
+  throw new Error('only S3/Minio storage can be initialized...');
+})();
 
 const s3 = {
   type: () => 's3',
